372daaee05b58cac45608fa3551136e0f12f5a37
hawang <654296217@qq.com>
2015-11-17 23:30:49 +0800

    change order of operation from integer to string

--- spec/test_operation.rb
+++ spec/test_operation.rb
@@ -6,21 +6,21 @@ describe Operation do
   describe 'when create a new operation' do
 
     before do
-      @operation_with_order = Operation.new('Admin', 'Import member', 1)
+      @operation_with_order = Operation.new('Admin', 'Import member', '1')
     end
 
     it 'should has role, name and order' do
       @operation_with_order.role.must_equal 'Admin'
       @operation_with_order.name.must_equal 'Import member'
-      @operation_with_order.order.must_equal 1
+      @operation_with_order.order.must_equal '1'
     end
   end
 
   describe 'when invoke inBelongToBaseline' do
 
     before do
-      @operation_with_order = Operation.new('Admin', 'Import member', 1)
-      @operation_without_order = Operation.new('Admin', 'Import member', nil)
+      @operation_with_order = Operation.new('Admin', 'Import member', '1')
+      @operation_without_order = Operation.new('Admin', 'Import member', '')
     end
 
     it 'given with order, should return true' do
