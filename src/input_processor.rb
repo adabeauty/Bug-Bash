e92b8c781d5b408b27a4cd8f6d32e1813fae7ff7
hawang <654296217@qq.com>
2015-11-18 11:16:24 +0800

    use magic number in inputProcessor

--- src/input_processor.rb
+++ src/input_processor.rb
@@ -1,26 +1,22 @@
 class InputProcessor
 
-  public
-  def self.getOperations
-    inputsArray = Array.new
+  ADMIN_OPERATIONS_FILE = '../data/admin_options.txt'
+  STUDENT_OPERATIONS_FILE = '../data/student_options.txt'
+  FACULTY_OPERATIONS_FILE = '../data/faculty_options.txt'
 
-    inputsArray.concat(readFile('../data/admin_options.txt'))
-    inputsArray.concat(readFile('../data/faculty_options.txt'))
-    inputsArray.concat(readFile('../data/student_options.txt'))
-
+  def self.getOperations  
+    inputsArray = []
+    inputsArray.concat(readFile(ADMIN_OPERATIONS_FILE))
+    inputsArray.concat(readFile(FACULTY_OPERATIONS_FILE))
+    inputsArray.concat(readFile(STUDENT_OPERATIONS_FILE))
     inputsArray
   end
 
   private
   def self.readFile(fileName)
     lines = IO.readlines(fileName)
-
     options = Array.new
-    lines.each do |line|
-      option = spliteLine(line)
-      options.push(option)
-    end
-
+    lines.each {|line| options << spliteLine(line)}
     options
   end
 
