9628c26f55b5f348a0494b0949954b43274833a9
hawang <654296217@qq.com>
2015-11-18 23:30:58 +0800

    :invoke print method

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
-# require File.dirname(__FILE__) + '/baseline'
-#
-# puts '------------------------------------------------- baseline -------------------------------------------------'
+require File.dirname(__FILE__) + '/src/optional_operation'
+require File.dirname(__FILE__) + '/src/operation'
+require File.dirname(__FILE__) + '/src/baseline'
+require File.dirname(__FILE__) + '/src/input_processor'
+
+puts '------------------------------------------------- baseline -------------------------------------------------'
 Baseline.print
+
+puts '------------------------------------------------- optional operation -------------------------------------------------'
+OptionalOperation.print(3)
